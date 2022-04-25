a680bbfbc6d91b759f5048c572aead6445f80c7d
Lorcan Nicholls <72615977+lorcan2440@users.noreply.github.com>
2022-04-26 00:11:55 +0100

    Update anim_diff_eq_solver.py

--- anim_diff_eq_solver.py
+++ anim_diff_eq_solver.py
@@ -2,7 +2,7 @@ import scipy.integrate
 from matplotlib import animation as anim, pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import math, sys
+import math, sys, time
 
 
 mpl.rcParams['animation.ffmpeg_path'] = r'C:\ffmpeg-build\bin\ffmpeg.exe'
@@ -11,11 +11,11 @@ plt.style.use(r'C:\Users\lnick\Documents\Personal\Programming\Python\Resources\p
 
 INITIAL_VALUES = np.array([10, 6])      # initial values of dependent variables
 INITIAL_T = 0                           # initial value of independent variable
-STEP_T = 1e-3                           # maximum integration step size
-ANIM_STEP_T = 0.1                       # update frame every interval
-SLIDING_WINDOW_T = 10                   # start moving along with the graph after this time
+STEP_T = 0.5                           # maximum integration step size
+ANIM_STEP_T = 5                       # update frame every interval
+SLIDING_WINDOW_T = 75                   # start moving along with the graph after this time
 DEP_VAR_NAMES = np.array(['Rabbits (prey)', 'Foxes (predators)'])    # shown in legend
-SAVE_OR_VIEW = 'save'                   # save: terminate program to end video recording, or
+SAVE_OR_VIEW = 'view'                   # save: terminate program to end video recording, or
                                         # view: watch graphing in progress
 
 def system_rhs(t: float, y_vec: np.ndarray) -> np.ndarray:
@@ -29,13 +29,19 @@ def system_rhs(t: float, y_vec: np.ndarray) -> np.ndarray:
     ])
 
 
-def animate(frame: int, history: dict[str, np.ndarray]) -> None:
+def init_graph():
 
     # set graphical options
     plt.cla()
     plt.title('Evolution of populations')
     plt.xlabel('Time / weeks')
     plt.ylabel('Population, in hundreds')
+
+
+def animate(frame: int, history: dict[str, np.ndarray]) -> None:
+
+    # clear
+    plt.cla()
 
     # the new values of t to be plotted in this frame
     t_range = np.array([INITIAL_T + STEP_T * frame * window, \
@@ -56,8 +62,8 @@ def animate(frame: int, history: dict[str, np.ndarray]) -> None:
 
     # chop down lists if needed
     if SLIDING_WINDOW_T is not None:
-        new_start_t = all_t[-1] - SLIDING_WINDOW_T
-        all_t = all_t[all_t >= new_start_t]
+        new_cutoff = all_t[-1] - SLIDING_WINDOW_T
+        all_t = all_t[all_t >= new_cutoff]
         all_y_vec = all_y_vec[:, -1 * len(all_t):]
         if all_t[-1] < INITIAL_T + SLIDING_WINDOW_T:
             plt.xlim(INITIAL_T, INITIAL_T + SLIDING_WINDOW_T)
@@ -66,28 +72,28 @@ def animate(frame: int, history: dict[str, np.ndarray]) -> None:
     history.update({'last_vals': last_vals, 'all_t': all_t, 'all_y_vec': all_y_vec})
 
     # plot lines up to the latest point
-    plt.ylim(0, 18)
     for i, y_vec in enumerate(all_y_vec):
         plt.plot(all_t, y_vec, label=DEP_VAR_NAMES[i])
 
     # show legend
     plt.legend(loc='upper left')
 
-	
+
 if __name__ == '__main__':
 
-	NUM_DEPENDENT_VARS = len(DEP_VAR_NAMES)
-	window = int(ANIM_STEP_T / STEP_T)
-	history = {
-		'last_vals': INITIAL_VALUES,
-		'all_t': np.array([]),
-		'all_y_vec': np.array([[] for _ in range(NUM_DEPENDENT_VARS)])
-		}
+    NUM_DEPENDENT_VARS = len(DEP_VAR_NAMES)
+    window = int(ANIM_STEP_T / STEP_T)
+    history = {
+        'last_vals': INITIAL_VALUES,
+        'all_t': np.array([INITIAL_T]),
+        'all_y_vec': np.array([[y_val] for y_val in INITIAL_VALUES])
+        }
 
-	fig = plt.figure()
-	ani = anim.FuncAnimation(fig, animate, fargs=(history,), interval=10, save_count=sys.maxsize)
+    fig = plt.figure()
+    ani = anim.FuncAnimation(fig, animate, fargs=(history,), init_func=init_graph, \
+                             interval=10, save_count=sys.maxsize)
 
-	if SAVE_OR_VIEW == 'view':
-		plt.show()
-	elif SAVE_OR_VIEW == 'save':
-		ani.save('MyVideo.mp4', writer=anim.FFMpegWriter(fps=30, codec='libx264', bitrate=-1))
+    if SAVE_OR_VIEW == 'view':
+        plt.show()
+    elif SAVE_OR_VIEW == 'save':
+        ani.save('MyVideo.mp4', writer=anim.FFMpegWriter(fps=30, codec='libx264', bitrate=-1))
